36df91339ea15f1be97da85a626868945c95572f
Daniel <102466020+danielCodingGuy@users.noreply.github.com>
2022-10-18 15:01:45 +0200

    Update main.py
    Groundwork for discord bot made in python, still in development

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ class MyClient(discord.Client):
             await message.reply('Hello!', mention_author=True)
 
         if message.content.startswith('$help'):
-            await message.reply('Command list: $hello',       mention_author=True)
+            await message.reply('Command list: $hello', mention_author=True)
 
 intents = discord.Intents.default()
 intents.message_content = True
