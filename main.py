16e72f2b5a0d092ed49d063ea6366afd58774aa2
Daniel <102466020+danielCodingGuy@users.noreply.github.com>
2022-10-25 18:14:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,9 +15,6 @@ class MyClient(discord.Client):
         if message.content.startswith('$hello'):
             await message.reply('Hello!', mention_author=True)
 
-        if message.content.startswith('$help'):
-            await message.reply('Command list: $hello', mention_author=True)
-
 intents = discord.Intents.default()
 intents.message_content = True
 
