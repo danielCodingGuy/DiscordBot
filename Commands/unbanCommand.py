4ce4d0fcab06f15b0a768a2569847f74f72c7712
Daniel <102466020+danielCodingGuy@users.noreply.github.com>
2023-01-28 18:16:24 +0100

    Update unbanCommand.py

--- Commands/unbanCommand.py
+++ Commands/unbanCommand.py
@@ -1,6 +1,8 @@
 #Command that can unban user that has been banned in the past
 #author: danielCodingGuy
 
+bot = Bot("$") #In this example you will need to use $ before command, use whatever you like in your bot.
+
 @bot.command(name='unban')
 async def _unban(ctx, id: int):
     user = await bot.fetch_user(id)
